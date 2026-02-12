ba78bc104b38859c1dc9d7e8b2e7afec05345271
Caguayo Developer <dev@caguayo.local>
2026-02-12 12:49:24 -0500

    Config: Frontend to run strictly on port 5173

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,8 @@ export default defineConfig({
   },
   server: {
     host: true, 
+    port: 5173,
+    strictPort: true,
     proxy: {
         '/api': {
             target: 'http://localhost:8000',
