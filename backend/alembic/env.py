b86d67c21f017643263428bb46c99b3e9a392bec
Pool <parispoool@protonmail.com>
2026-06-17 10:37:44 -0400

    fix alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -11,8 +11,7 @@ from src.database.connection import DATABASE_URL
 from src.models import SQLModel
 
 config = context.config
-if not config.get_main_option("sqlalchemy.url"):
-    config.set_main_option("sqlalchemy.url", DATABASE_URL)
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
